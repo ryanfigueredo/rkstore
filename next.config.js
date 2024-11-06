2ef5bfda14717da4f23bd6446aa082612c978180
Ryan <ryan.figueredo059@gmail.com>
2024-11-06 00:31:05 -0300

    refactor: update image domain and remove unused deals API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["shopimagesrc.s3.us-east-2.amazonaws.com"],
+    domains: ["utfs.io"],
   },
   experimental: {
     serverActions: true,
